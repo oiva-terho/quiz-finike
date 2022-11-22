0d79b4a617ed2d8e7eca8bf6f4b866731ab7b34c
oiva-terho <oiva.terho@gmail.com>
2022-11-22 23:10:49 +0300

    styles fixes

--- src/components/photo/photo.component.tsx
+++ src/components/photo/photo.component.tsx
@@ -4,6 +4,6 @@ import './photo.styles.scss';
 
 export const Photo: FC<ImgHTMLAttributes<HTMLImageElement>> = ({ src }) => (
   <div className='photo'>
-    <img src={src} alt='quiz' />
+    <img src={src} alt='quiz' loading='lazy' />
   </div>
 );
