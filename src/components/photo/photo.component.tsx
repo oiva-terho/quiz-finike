4be939b189ce22febad2bf16fc1ce54aa1dcdc6c
oiva-terho <oiva.terho@gmail.com>
2022-12-21 16:39:25 +0300

    overflow fix

--- src/components/photo/photo.component.tsx
+++ src/components/photo/photo.component.tsx
@@ -4,11 +4,13 @@ import './photo.styles.scss';
 
 export const Photo: FC<ImgHTMLAttributes<HTMLImageElement>> = ({ src }) => {
   const [zoom, setZoom] = useState(false);
+  const body = document.querySelector('body');
+  const handleZoom = () => {
+    if (body) body.style.overflow = `${zoom ? '' : 'hidden'}`;
+    setZoom(zoom ? false : true);
+  };
   return (
-    <button
-      className={zoom ? 'photo__backing' : 'photo'}
-      onClick={() => setZoom(zoom ? false : true)}
-    >
+    <button className={zoom ? 'photo__backing' : 'photo'} onClick={handleZoom}>
       <img src={src} alt='quiz' />
     </button>
   );
