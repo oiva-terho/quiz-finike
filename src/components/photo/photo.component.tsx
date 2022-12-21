8c8ce7fdf51f39e295f53a744da551cc1d578046
oiva-terho <oiva.terho@gmail.com>
2022-12-21 15:27:04 +0300

    zoom image ok click
    known bug:
    - overflow: hidden doesn't work

--- src/components/photo/photo.component.tsx
+++ src/components/photo/photo.component.tsx
@@ -1,9 +1,15 @@
-import { FC, ImgHTMLAttributes } from 'react';
+import { FC, ImgHTMLAttributes, useState } from 'react';
 
 import './photo.styles.scss';
 
-export const Photo: FC<ImgHTMLAttributes<HTMLImageElement>> = ({ src }) => (
-  <div className='photo'>
-    <img src={src} alt='quiz' />
-  </div>
-);
+export const Photo: FC<ImgHTMLAttributes<HTMLImageElement>> = ({ src }) => {
+  const [zoom, setZoom] = useState(false);
+  return (
+    <button
+      className={zoom ? 'photo__backing' : 'photo'}
+      onClick={() => setZoom(zoom ? false : true)}
+    >
+      <img src={src} alt='quiz' />
+    </button>
+  );
+};
