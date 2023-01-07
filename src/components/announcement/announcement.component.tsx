5638e4a4956c6328c2d129a90f44f14816d6229b
oiva-terho <oiva.terho@gmail.com>
2023-01-07 13:47:46 -0300

    announcement date count
    - added english
    - added count of next friday
    - removed decor elements

--- src/components/announcement/announcement.component.tsx
+++ src/components/announcement/announcement.component.tsx
@@ -1,9 +1,43 @@
+import { useTranslation } from 'react-i18next';
+
 import { ReactComponent as MapLogo } from '~/assets/map.svg';
 import './announcement.styles.scss';
 
 export const Announcement = () => {
+  const { t, i18n } = useTranslation('translation', { keyPrefix: 'announcement' });
+
+  const today = new Date();
+
+  const nextGame = () => {
+    const nextFriday = () => {
+      const weekday = today.getDay();
+      if (weekday === 5) return today;
+      return new Date(
+        today.setDate(today.getDate() + (5 - weekday < 0 ? 5 - weekday + 7 : 5 - weekday)),
+      );
+    };
+    const month = nextFriday().getMonth().toString();
+    const day = nextFriday().getDate();
+    const ordinal = (number: number) => {
+      switch (number) {
+        case 1:
+          return 'st';
+        case 2:
+          return 'nd';
+        case 3:
+          return 'rd';
+        default:
+          return 'th';
+      }
+    };
+    return i18n.resolvedLanguage === 'en'
+      ? `${t(`months.${month}`)} ${day}${ordinal(day)}`
+      : `${day} ${t(`months.${month}`)}`;
+  };
+
   return (
     <div className='announcement'>
+      <h3>{t('nextGame')}</h3>
       <div className='announcement__continetns'>
         <img src='/assets/announcement/nthamerica.webp' alt='' />
         <img src='/assets/announcement/sthamerica.webp' alt='' />
@@ -17,23 +51,18 @@ export const Announcement = () => {
         <div />
       </div>
       <div className='announcement__plate'>
-        <h2>21 октября</h2>
-        <div>ПЯТНИЦА | 19:30</div>
+        <h2>{nextGame()}</h2>
+        <div>{t('friday')} | 19:30</div>
         <a href='https://goo.gl/maps/qYo4d3hvxq9kt3a69'>
           <MapLogo />
-          &nbsp; Hanımeli cafe
+          Hanımeli Cafe
         </a>
         <div>
-          Стоимость участия <span>70₺</span>
+          {t('cost')}
+          <span>70₺</span>
         </div>
       </div>
-      <a href='https://t.me/finikequiz'>t.me/finikequiz</a>
-      <div className='announcement__decor'>
-        <img src='/assets/announcement/rocks.webp' alt='' />
-        <img src='/assets/announcement/leafs-bk.webp' alt='' />
-        <img src='/assets/announcement/oranges.webp' alt='' />
-        <img src='/assets/announcement/leafs-fr.webp' alt='' />
-      </div>
+      <a href='https://t.me/finikequiz'>{t('play')}</a>
     </div>
   );
 };
--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -3,17 +3,17 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { Button } from '~/components/button/button.component';
+import { selectCurrentUser } from '~/store/user/user.selector';
 import { clearGame } from '~/store/game/game.action';
+import { clearPhotos } from '~/store/gallery/gallery.action';
 import { signOutStart } from '~/store/user/user.action';
 
 import { ReactComponent as QuizLogo } from '~/assets/finike-quiz.svg';
 import { ReactComponent as GamesLogo } from '~/assets/results.svg';
 import { ReactComponent as GalleryLogo } from '~/assets/gallery.svg';
 import { ReactComponent as AuthLogo } from '~/assets/auth.svg';
-import { selectCurrentUser } from '~/store/user/user.selector';
 
 import './navigation.styles.scss';
-import { clearPhotos } from '~/store/gallery/gallery.action';
 
 export const Navigation = () => {
   const dispatch = useDispatch();
