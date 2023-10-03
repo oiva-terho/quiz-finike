89b6041114a4ca40fcad2fe35af48eecdd318cb8
oiva.terho <oiva.terho@gmail.com>
2023-10-03 18:42:08 +0200

    last try to fix site preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 
 import { Spinner } from './components/spinner/spinner.component';
 
@@ -14,59 +13,8 @@ const SignUpForm = lazy(() => import('./routes/sign-up/sign-up.component'));
 const Statistics = lazy(() => import('./routes/statistics/statistics.component'));
 
 function App() {
-  const host = window.location.hostname;
-  const meta = {
-    title: 'Quiz in Finike',
-    description:
-      'Квиз в Финике. Посмотрите статистику и фото интеллектуальной коммандной игры для русскоязычных в Турции',
-    keywords: ['quiz', 'intellectual', 'Turkey', 'Finike', 'team game'],
-    viewport: { width: 'device-width', initialScale: 1 },
-    openGraph: {
-      type: 'website',
-      url: 'https://quiz-finike.web.app/',
-      title: 'Quiz Finike',
-      description:
-        'Квиз в Финике, Турция. Посмотрите статистику и фото интеллектуальной коммандной игры для русскоязычных в Турции',
-      siteName: 'Quiz Finike',
-      images: `https://${host}/preview.png`,
-    },
-    twitter: {
-      title: 'Quiz Finike',
-      description:
-        'Квиз в Финике, Турция. Посмотрите статистику и фото интеллектуальной коммандной игры для русскоязычных в Турции',
-      card: 'summary_large_image',
-      site: '@site',
-      creator: '@creator',
-      images: `https://${host}/preview.png`,
-    },
-  };
   return (
     <Suspense fallback={<Spinner />}>
-      <Helmet>
-        <title>{meta.title}</title>
-        <meta name='description' content={meta.description} />
-        <meta name='keywords' content={meta.keywords.join(', ')} />
-        <meta
-          name='viewport'
-          content={`width=${meta.viewport.width}, initial-scale=${meta.viewport.initialScale}`}
-        />
-
-        {/* OpenGraph */}
-        <meta property='og:type' content={meta.openGraph.type} />
-        <meta property='og:url' content={meta.openGraph.url} />
-        <meta property='og:title' content={meta.openGraph.title} />
-        <meta property='og:description' content={meta.openGraph.description} />
-        <meta property='og:site_name' content={meta.openGraph.siteName} />
-        <meta property='og:image' content={meta.openGraph.images} />
-
-        {/* Twitter */}
-        <meta name='twitter:title' content={meta.twitter.title} />
-        <meta name='twitter:description' content={meta.twitter.description} />
-        <meta name='twitter:card' content={meta.twitter.card} />
-        <meta name='twitter:site' content={meta.twitter.site} />
-        <meta name='twitter:creator' content={meta.twitter.creator} />
-        <meta name='twitter:image' content={meta.twitter.images} />
-      </Helmet>
       <Routes>
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
